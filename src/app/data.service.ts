88e761ac04a7364e0789962e3ccfb040b40d592c
campar <kralj.david.david@gmail.com>
2019-09-19 23:32:12 +0200

    Fix json property names and path

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,34 +2,33 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 
-const url = 'http://localhost:8080';
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  private url = 'http://localhost:8080';
+
   constructor(private http: HttpClient) { }
 
-  // public url: 'http://localhost:8080';
-
   getUniversities() {
-    return this.http.get(`${url}/university`);
+    return this.http.get(`${this.url}/university`);
   }
 
   getFacultiesByUniversityId(id) {
-    return this.http.get(`${url}/university/${id}/faculties`);
+    return this.http.get(`${this.url}/university/${id}/faculties`);
   }
 
   getSubjectById(id) {
-    return this.http.get(`${url}/subjects/${id}`);
+    return this.http.get(`${this.url}/subjects/${id}`);
   }
 
   getFacultyDetails(id) {
-    return this.http.get(`${url}/faculties/${id}`);
+    return this.http.get(`${this.url}/faculties/${id}`);
   }
 
   getStudyPrograme(id) {
-    return this.http.get(`${url}/study-programmes/${id}`);
+    return this.http.get(`${this.url}/study-programmes/${id}`);
   }
 
 }
